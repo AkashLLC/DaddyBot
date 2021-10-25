08ae0c131c36d1e24b3ab3f9d2b000b09f077fbb
AkashLLC <92622881+AkashLLC@users.noreply.github.com>
2021-10-25 18:02:04 +0530

    Update ow.js

--- plugins/ow.js
+++ plugins/ow.js
@@ -18,7 +18,7 @@ const ENSON = "```.``` "
 // Plugin Start 
 if (con.LANG === 'EN' || 'az') {
 
-    if (con.WORKTYPE === 'private') {
+    if (con.WORKTYPE === 'public') {
 
         Asena.addCommand({pattern: 'own', fromMe: true, desc: ENZAR}, (async (message, match) => {
 
@@ -29,7 +29,7 @@ if (con.LANG === 'EN' || 'az') {
             var r_text = new Array ();
             r_text[0] = "*OWNER List*\n*➣antilink  on/off*\n*➣ fulleva on/off*\n*➣ mlink on/of*\n*➣ theri no/yes*\n*➣ update now*\n*➣ tagall*\n*➣ bgm on/off*";
             r_text[1] = "*Heroku Settings*\n*➣ BOT_NAME*\n*➣ REMOVE_BG_API_KEY*\n*➣ TAG_HEADER*\n*➣ TAG_REPLY*\n*➣ SUDO*\n*➣ WORK_TYPE*\n*➣ OWNER_NAME*\n*➣ LANGUAGE*\n*➣ KICKME_MESSAGE*\n*➣ AUTO_BİO*\n*➣ ALIVE_MESSAGE*\n*➣ ALL_CAPTION*";
-            r_text[2] = "*HOW TO USE HEROKU COMMAND IN GROUP*\n*➣ setvar BOT_NAME: [💘Paathu💘]*\n*➣ setvar WORK_TYPE:public*\n*➣ setvar SUDO:91xxxxxxxxxx,0,91xxxxxxxxxx*\n*➣ setvar LANGUAGE:en*";
+            r_text[2] = "*HOW TO USE HEROKU COMMAND IN GROUP*\n*➣ setvar BOT_NAME: [💘Daddy👴]*\n*➣ setvar WORK_TYPE:public*\n*➣ setvar SUDO:91xxxxxxxxxx,0,91xxxxxxxxxx*\n*➣ setvar LANGUAGE:en*";
             r_text[3] = "setvar BOT_NAME:[💘Daddy💘]";
             r_text[4] = "antilink on";
             r_text[5] = "*ennum sneham mathram😊*";
@@ -51,8 +51,8 @@ if (con.LANG === 'EN' || 'az') {
             var r_text = new Array ();
             r_text[0] = "*OWNER List*\n*➣antilink  on/off*\n*➣ fulleva on/off*\n*➣ mlink on/of*\n*➣ theri no/yes*\n*➣ update now*\n*➣ tagall*\n*➣ bgm on/off*";
             r_text[1] = "*Heroku Settings*\n*➣ BOT_NAME*\n*➣ REMOVE_BG_API_KEY*\n*➣ TAG_HEADER*\n*➣ TAG_REPLY*\n*➣ SUDO*\n*➣ WORK_TYPE*\n*➣ OWNER_NAME*\n*➣ LANGUAGE*\n*➣ KICKME_MESSAGE*\n*➣ AUTO_BİO*\n*➣ ALIVE_MESSAGE*\n*➣ ALL_CAPTION*";
-            r_text[2] = "*HOW TO USE HEROKU COMMAND IN GROUP*\n*➣ setvar BOT_NAME: [💘Paathu💘]*\n*➣ setvar WORK_TYPE:public*\n*➣ setvar SUDO:91xxxxxxxxxx,0,91xxxxxxxxxx*\n*➣ setvar LANGUAGE:en*";
-            r_text[3] = ".setvar BOT_NAME:[💘Paathu💘]";
+            r_text[2] = "*HOW TO USE HEROKU COMMAND IN GROUP*\n*➣ setvar BOT_NAME: [💘Daddy💘]*\n*➣ setvar WORK_TYPE:public*\n*➣ setvar SUDO:91xxxxxxxxxx,0,91xxxxxxxxxx*\n*➣ setvar LANGUAGE:en*";
+            r_text[3] = ".setvar BOT_NAME:[💘Daddy💘]";
             r_text[4] = ".antilink on";
             r_text[5] = "*aennum sneham mathram😊*";
 
